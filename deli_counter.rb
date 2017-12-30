58936573c6b3629ae5268234d751d2f07327b0e4
Daiane Bushey <daianebushey@gmail.com>
2017-12-30 04:38:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,4 +3,8 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length === 0
     return "The line is currently empty."
+  else
+    katz_deli.each_with_index.map do |client, index|
+    return "The line is currently: #{index + 1}. #{client}"
+  end
 end
