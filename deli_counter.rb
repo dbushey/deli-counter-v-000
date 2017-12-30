25bac27475c011d5fb817c5476d315e111d83e04
Daiane Bushey <daianebushey@gmail.com>
2017-12-30 05:34:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,22 @@
 
 def line(katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     index = 0
     while index != katz_deli.length do
       index += 1
       new_array.push("#{index}. #{katz_deli[index]}")
     end
-    return "The line is currently:#{new_array}"
+    puts "The line is currently:#{new_array}"
   end
 end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name);
-  "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+
 end
