21952b27671bbe00760b6db10facff5fbde672b1
Daiane Bushey <daianebushey@gmail.com>
2017-12-30 05:43:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,8 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    puts "Currently serving #{katz_deli.shift(1)}."
 end
