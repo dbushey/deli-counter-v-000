2dc78aa27b6e8044a55c89d6088c9346e9a4af51
Daiane Bushey <daianebushey@gmail.com>
2017-12-30 05:30:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,14 @@
 
 def line(katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    "The line is currently empty."
   else
     index = 0
     while index != katz_deli.length do
       index += 1
       new_array.push("#{index}. #{katz_deli[index]}")
     end
-    return "The line is currently:#{new_array}"
+    "The line is currently:#{new_array}"
   end
 end
 
