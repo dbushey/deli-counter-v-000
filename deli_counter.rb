d60f660166133d489c5a65f1b474af7e006f0794
Daiane Bushey <daianebushey@gmail.com>
2017-12-30 05:13:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,6 @@
 
 def line(katz_deli)
+
   if katz_deli.length === 0
     return "The line is currently empty."
   else
@@ -10,4 +11,5 @@ def line(katz_deli)
     end
     return "The line is currently:#{new_array}"
   end
+  
 end
